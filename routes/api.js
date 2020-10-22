915107e372338f3eeecfae913433f00207433648
Kabirdas Henry <kbrdsk@gmail.com>
2020-10-22 10:44:32 -0700

    adding new user returns jwt and user info

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,14 @@
+require("dotenv").config();
+
 const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const apiRouter = express.Router();
 
-apiRouter.get("/", (req, res) => {
-	res.json({message: "apiRouter functional"});
-})
+apiRouter.post("/students", (req, res) => {
+	const student = { first: req.body.first, last: req.body.last };
+	const token = jwt.sign({ student }, process.env.JWTSECRET);
+	res.json({ student, token });
+});
 
 module.exports = apiRouter;
