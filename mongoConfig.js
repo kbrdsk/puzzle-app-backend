b4dfd4c3507057a60c170dcaccb3e2036694518c
Kabirdas Henry <kbrdsk@gmail.com>
2020-10-26 15:35:29 -0700

    update mongoose options

--- mongoConfig.js
+++ mongoConfig.js
@@ -1,7 +1,12 @@
-const mongoose = requier("mongoose");
+const mongoose = require("mongoose");
 
 const mongoDb = process.env.MONGOURL;
+const mongooseOpts = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+};
 
-mongoose.connect(mongoDb, { useNewUrlParser: true });
+mongoose.connect(mongoDb, mongooseOpts);
 const db = mongoose.connect;
 db.on("erro", console.error.bind(console, "mongo connection error"));
