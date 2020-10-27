b471d2088ae4e6d6b58937e933ba310f0c5aad47
Kabirdas Henry <kbrdsk@gmail.com>
2020-10-26 18:40:54 -0700

    testdb now initialized properly

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ const cors = require("cors");
 
 const apiRouter = require("./routes/api");
 
-require("./mongoConfigTesting");
+const { dbSetup } = require("./mongoConfigTesting");
 
-const app = express();
+dbSetup().then(() => {
+	const app = express();
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(cors());
-app.use("/api", apiRouter);
+	app.use(express.urlencoded({ extended: false }));
+	app.use(express.json());
+	app.use(cors());
+	app.use("/api", apiRouter);
 
-app.listen(process.env.PORT, () =>
-	console.log(`App listening on port ${process.env.PORT}`)
-);
+	app.listen(process.env.PORT, () =>
+		console.log(`App listening on port ${process.env.PORT}`)
+	);
+});
