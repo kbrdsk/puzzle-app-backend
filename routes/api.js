7ff293eca03b862edb210f336cc35f6e90a2abdf
Kabirdas Henry <kbrdsk@gmail.com>
2020-10-26 18:16:25 -0700

    added generic error msgs

--- routes/api.js
+++ routes/api.js
@@ -9,29 +9,37 @@ const puzzles = require("../models/puzzles/index");
 const apiRouter = express.Router();
 
 apiRouter.post("/students", async (req, res) => {
-	const studentData = {
-		first: req.body.first.toLowerCase(),
-		last: req.body.last.toLowerCase(),
-	};
-	if (await Student.findOne(studentData)) {
-		res.sendStatus(403);
-	} else {
-		const student = new Student(studentData);
-		student.save();
-		const token = jwt.sign({ student }, process.env.JWTSECRET);
-		res.json({ token });
+	try {
+		const studentData = {
+			first: req.body.first.toLowerCase(),
+			last: req.body.last.toLowerCase(),
+		};
+		if (await Student.findOne(studentData)) {
+			res.sendStatus(403);
+		} else {
+			const student = new Student(studentData);
+			student.save();
+			const token = jwt.sign({ student }, process.env.JWTSECRET);
+			res.json({ token });
+		}
+	} catch {
+		res.status(500).send();
 	}
 });
 
 apiRouter.post("/students/login", async (req, res) => {
-	const first = req.body.first.toLowerCase();
-	const last = req.body.last.toLowerCase();
-	const student = await Student.findOne({ first, last });
-	if (!student) {
-		res.status(404).send();
-	} else {
-		const token = jwt.sign({ student }, process.env.JWTSECRET);
-		res.json({ token });
+	try {
+		const first = req.body.first.toLowerCase();
+		const last = req.body.last.toLowerCase();
+		const student = await Student.findOne({ first, last });
+		if (!student) {
+			res.status(404).send();
+		} else {
+			const token = jwt.sign({ student }, process.env.JWTSECRET);
+			res.json({ token });
+		}
+	} catch {
+		res.status(500).send();
 	}
 });
 
@@ -39,19 +47,25 @@ apiRouter.get(
 	"/puzzles/:puzzleName/:puzzleId",
 	verifyToken,
 	async (req, res) => {
-		const { puzzleName, puzzleId } = req.params;
-		const student = await Student.findOne(req.student);
-		if (!student) res.status(404).send();
-		else {
-			let puzzle = await puzzles[puzzleName].Puzzle.findOne({
-				puzzleId: puzzleId,
-				student: student._id,
-			});
-			if (!puzzle) {
-				puzzle = await puzzles[puzzleName].defaults[puzzleId](student);
-				await puzzle.save();
+		try {
+			const { puzzleName, puzzleId } = req.params;
+			const student = await Student.findOne(req.student);
+			if (!student) res.status(404).send();
+			else {
+				let puzzle = await puzzles[puzzleName].Puzzle.findOne({
+					puzzleId: puzzleId,
+					student: student._id,
+				});
+				if (!puzzle) {
+					puzzle = await puzzles[puzzleName].defaults[puzzleId](
+						student
+					);
+					await puzzle.save();
+				}
+				res.json(puzzle);
 			}
-			res.json(puzzle);
+		} catch {
+			res.status(500).send();
 		}
 	}
 );
@@ -59,23 +73,27 @@ apiRouter.put(
 	"/puzzles/:puzzleName/:puzzleId",
 	verifyToken,
 	async (req, res) => {
-		const { puzzleName, puzzleId } = req.params;
-		const student = await Student.findOne(req.student);
-		if (!student) return res.status(404).send();
-		let puzzle = await puzzles[puzzleName].Puzzle.findOne({
-			puzzleId,
-			student: student._id,
-		});
-		if (!puzzle) {
-			puzzle = await puzzles[puzzleName].defaults[puzzleId](student);
-			await puzzle.save();
+		try {
+			const { puzzleName, puzzleId } = req.params;
+			const student = await Student.findOne(req.student);
+			if (!student) return res.status(404).send();
+			let puzzle = await puzzles[puzzleName].Puzzle.findOne({
+				puzzleId,
+				student: student._id,
+			});
+			if (!puzzle) {
+				puzzle = await puzzles[puzzleName].defaults[puzzleId](student);
+				await puzzle.save();
+			}
+			puzzle = await puzzles[puzzleName].Puzzle.findByIdAndUpdate(
+				puzzle._id,
+				{ work: req.body.puzzleData },
+				{ new: true }
+			);
+			res.json(puzzle);
+		} catch {
+			res.status(500).send();
 		}
-		puzzle = await puzzles[puzzleName].Puzzle.findByIdAndUpdate(
-			puzzle._id,
-			{ work: req.body.puzzleData },
-			{ new: true }
-		);
-		res.json(puzzle);
 	}
 );
 
