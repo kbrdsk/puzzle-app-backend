e5ef8dd0dfda6fc3e42af03cc1cf8eca1f979420
Kabirdas Henry <kbrdsk@gmail.com>
2020-11-03 10:10:25 -0800

    added second puzzle

--- models/puzzles/calcudoku.js
+++ models/puzzles/calcudoku.js
@@ -104,6 +104,117 @@ const defaults = {
 				},
 			],
 		}),
+	"sample2": (student) =>
+		Puzzle({
+			puzzleId: "sample2",
+			title: "Sample Two",
+			student: student._id,
+			work: [],
+			size: 5,
+			cages: [
+				{
+					operation: "+",
+					result: 8,
+					squares: [
+						{ col: 0, row: 0 },
+						{ col: 0, row: 1 },
+					],
+				},
+				{
+					operation: "+",
+					result: 9,
+					squares: [
+						{ col: 1, row: 0 },
+						{ col: 2, row: 0 },
+					],
+				},
+				{
+					operation: "+",
+					result: 3,
+					squares: [
+						{ col: 1, row: 1 },
+						{ col: 2, row: 1 },
+					],
+				},
+				{
+					operation: "+",
+					result: 3,
+					squares: [
+						{ col: 3, row: 0 },
+						{ col: 4, row: 0 },
+					],
+				},
+				{
+					operation: "",
+					result: 5,
+					squares: [{ col: 2, row: 2 }],
+				},
+				{
+					operation: "+",
+					result: 7,
+					squares: [
+						{ col: 0, row: 2 },
+						{ col: 1, row: 2 },
+					],
+				},
+				{
+					operation: "+",
+					result: 8,
+					squares: [
+						{ col: 3, row: 3 },
+						{ col: 4, row: 3 },
+					],
+				},
+				{
+					operation: "+",
+					result: 5,
+					squares: [
+						{ col: 1, row: 3 },
+						{ col: 2, row: 3 },
+					],
+				},
+				{
+					operation: "+",
+					result: 5,
+					squares: [
+						{ col: 1, row: 4 },
+						{ col: 2, row: 4 },
+					],
+				},
+				{
+					operation: "+",
+					result: 9,
+					squares: [
+						{ col: 3, row: 4 },
+						{ col: 4, row: 4 },
+					],
+				},
+				{
+					operation: "+",
+					result: 3,
+					squares: [
+						{ col: 0, row: 3 },
+						{ col: 0, row: 4 },
+					],
+				},
+				{
+					operation: "+",
+					result: 3,
+					squares: [
+						{ col: 3, row: 1 },
+						{ col: 3, row: 2 },
+					],
+				},
+				{
+					operation: "+",
+					result: 3,
+					squares: [
+						{ col: 4, row: 1 },
+						{ col: 4, row: 2 },
+					],
+				},
+			],
+		}),
 };
 
 module.exports = { Puzzle, defaults };
