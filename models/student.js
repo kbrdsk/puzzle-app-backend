ef3936ac3e56e6695804ba9ae416c08790dc5c6c
Kabirdas Henry <kbrdsk@gmail.com>
2020-11-03 11:13:51 -0800

    added field for active puzzle

--- models/student.js
+++ models/student.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
 const StudentSchema = new Schema({
 	first: { type: String, required: true },
 	last: { type: String, required: true },
+	activepuzzle: { puzzleName: String, puzzleId: String },
 });
 
 StudentSchema.virtual("url").get(function () {
