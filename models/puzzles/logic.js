64e4b3c14f987f8e0057ca75ee62457a1aa093dd
kbrdsk <52506986+kbrdsk@users.noreply.github.com>
2020-11-11 10:29:23 -0800

    Create logic.js

---
+++ models/puzzles/logic.js
@@ -0,0 +1,15 @@
+ 
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PuzzleSchema = new Schema({
+	puzzleId: { type: String, required: true },
+	title: { type: String, required: true },
+	student: { type: Schema.Types.ObjectId, ref: "Student" },
+	work: String,
+  description: String,
+	source: String,
+	default: Boolean,
+});
+
+module.exports = mongoose.model("Logic", PuzzleSchema);
