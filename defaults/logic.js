f32681a5a40e7e15c7437c80640dbb9b87f674a4
Kabirdas Henry <kbrdsk@gmail.com>
2020-11-11 11:02:37 -0800

    linted

--- defaults/logic.js
+++ defaults/logic.js
@@ -3,10 +3,10 @@ module.exports.defaults = [
     populateID: "logic1",
     puzzleId: "wolfgoatcabbage",
     title: "Wolf, Goat, Cabbage",
-    description: "Once upon a time a farmer went to a market and purchased a wolf, a goat, and some cabbage. On her way home, the farmer came to the bank of a river and rented a boat. But crossing the river by boat, the farmer could carry only herself and a single one of her purchases: the wolf, the goat, or the cabbage.
+    description: `Once upon a time a farmer went to a market and purchased a wolf, a goat, and some cabbage. On her way home, the farmer came to the bank of a river and rented a boat. But crossing the river by boat, the farmer could carry only herself and a single one of her purchases: the wolf, the goat, or the cabbage.
 If left unattended together, the wolf would eat the goat, or the goat would eat the cabbage.
 The farmer's challenge was to carry herself and her purchases to the far bank of the river, leaving each purchase intact. How did she do it?
-",
+`,
     default: true,
     puzzleName: "logic",
     student: null,
@@ -15,7 +15,8 @@ The farmer's challenge was to carry herself and her purchases to the far bank of
     populateID: "logic2",
     puzzleId: "torch",
     title: "Bridge & Torch",
-    description: "Four people come to a river in the night. There is a narrow bridge, but it can only hold two people at a time. They have one torch and, because it's night, the torch has to be used when crossing the bridge. Person A can cross the bridge in 1 minute, B in 2 minutes, C in 5 minutes, and D in 8 minutes. When two people cross the bridge together, they must move at the slower person's pace. The question is, can they all get across the bridge if the torch lasts only 15 minutes?",
+    description:
+      "Four people come to a river in the night. There is a narrow bridge, but it can only hold two people at a time. They have one torch and, because it's night, the torch has to be used when crossing the bridge. Person A can cross the bridge in 1 minute, B in 2 minutes, C in 5 minutes, and D in 8 minutes. When two people cross the bridge together, they must move at the slower person's pace. The question is, can they all get across the bridge if the torch lasts only 15 minutes?",
     default: true,
     puzzleName: "logic",
     student: null,
@@ -24,14 +25,14 @@ The farmer's challenge was to carry herself and her purchases to the far bank of
     populateID: "logic3",
     puzzleId: "goldboxes",
     title: "Find the Gold",
-    description: "There are 3 boxes on a table, one box contains some Gold while the two are empty. Each box has a message written on it as follows:
-/n/n
-Box 1: The gold is not in here/n
-Box 2: The gold is not in here/n
-Box 3: The gold is in Box 2/n
-/n/n
+    description: `There are 3 boxes on a table, one box contains some Gold while the two are empty. Each box has a message written on it as follows:
+
+Box 1: The gold is not in here
+Box 2: The gold is not in here
+Box 3: The gold is in Box 2
+
 Only one of the messages is true, the other two are lies, which box actually contains the gold?
-",
+`,
     default: true,
     puzzleName: "logic",
     student: null,
@@ -40,9 +41,9 @@ Only one of the messages is true, the other two are lies, which box actually con
     populateID: "logic4",
     puzzleId: "socks",
     title: "Cathy's Socks",
-    description: "Cathy has twelve black socks and twelve white socks in her drawer.
+    description: `Cathy has twelve black socks and twelve white socks in her drawer.
 In complete darkness, and without looking, how many socks must she take from the drawer in order to be sure to get a pair that match?
-",
+`,
     default: true,
     puzzleName: "logic",
     student: null,
@@ -51,7 +52,8 @@ In complete darkness, and without looking, how many socks must she take from the
     populateID: "logic5",
     puzzleId: "10coins",
     title: "10 Coins",
-    description: "You are blindfolded and 10 coins are placed in front of you on a table. You are allowed to touch the coins, but can’t tell which face is up by feel. You are told that there are 5 coins heads up, and 5 coins tails up but not which ones are which. How do you make two piles of coins each with the same number of heads facing up? You can flip the coins any number of times.",
+    description:
+      "You are blindfolded and 10 coins are placed in front of you on a table. You are allowed to touch the coins, but can’t tell which face is up by feel. You are told that there are 5 coins heads up, and 5 coins tails up but not which ones are which. How do you make two piles of coins each with the same number of heads facing up? You can flip the coins any number of times.",
     default: true,
     puzzleName: "logic",
     student: null,
@@ -60,14 +62,14 @@ In complete darkness, and without looking, how many socks must she take from the
     populateID: "logic6",
     puzzleId: "applesandoranges",
     title: "Apples & Oranges",
-    description: "There are 3 crates in front of you. One crate contains only apples. One crate contains only oranges. The other crate contains both apples and oranges.
-/n/n
+    description: `There are 3 crates in front of you. One crate contains only apples. One crate contains only oranges. The other crate contains both apples and oranges.
+
 Each crate is labeled. One reads "apples", one reads "oranges", and one reads "apples and oranges".
-/n/n
+
 However, each crate has the wrong label on it.
-/n/n
+
 If you can only take out and look at just one of the pieces of fruit from just one of the crates, how can you label ALL of the crates correctly?
-",
+`,
     default: true,
     puzzleName: "logic",
     student: null,
@@ -76,14 +78,14 @@ If you can only take out and look at just one of the pieces of fruit from just o
     populateID: "logic7",
     puzzleId: "averybobbycam",
     title: "Avery, Bobby, & Cam",
-    description: "There are three siblings: Avery, Bobby, and Cam. One of them has eaten the last cookie. When asked about it they say the following:
-	/n/n
-	Avery: I didn’t eat the cookie./n
-	Bobby: Avery didn’t eat the cookie./n
-	Cam: I ate the cookie./n
-/n/n
+    description: `There are three siblings: Avery, Bobby, and Cam. One of them has eaten the last cookie. When asked about it they say the following:
+  
+  Avery: I didn’t eat the cookie.
+  Bobby: Avery didn’t eat the cookie.
+  Cam: I ate the cookie.
+
 As it turns out, the one who ate the cookie is lying. Who ate the cookie?
-",
+`,
     default: true,
     puzzleName: "logic",
     student: null,
@@ -92,8 +94,8 @@ As it turns out, the one who ate the cookie is lying. Who ate the cookie?
     populateID: "logic8",
     puzzleId: "childrenboat",
     title: "2 Adults, 2 Children, a Boat, and a River",
-    description: "Two adults of equal weight, together with two children, each of half their weight, wish to cross a river using a boat which can only carry the weight of one adult. How can they do it?
-",
+    description: `Two adults of equal weight, together with two children, each of half their weight, wish to cross a river using a boat which can only carry the weight of one adult. How can they do it?
+`,
     default: true,
     puzzleName: "logic",
     student: null,
@@ -102,7 +104,8 @@ As it turns out, the one who ate the cookie is lying. Who ate the cookie?
     populateID: "logic9",
     puzzleId: "bear",
     title: "Bear",
-    description: "A bear walks a mile south, then a mile east, then a mile north, and ends up in the exact same place they started in. What color is the bear?",
+    description:
+      "A bear walks a mile south, then a mile east, then a mile north, and ends up in the exact same place they started in. What color is the bear?",
     default: true,
     puzzleName: "logic",
     student: null,
@@ -111,7 +114,8 @@ As it turns out, the one who ate the cookie is lying. Who ate the cookie?
     populateID: "logic10",
     puzzleId: "rockpaperscissors",
     title: "Rock, Paper, Scissors",
-    description: "Angel and Bianca played rock paper scissors 10 times. We know how many times each of them threw rock, paper, and scissors, but we don’t know the order, all we know is that there were no ties. Angel threw rock 2 times, paper 4 times, and scissors 4 times. Bianca threw rock 3 times, paper 1 time, and scissors 6 times. What was the score?",
+    description:
+      "Angel and Bianca played rock paper scissors 10 times. We know how many times each of them threw rock, paper, and scissors, but we don’t know the order, all we know is that there were no ties. Angel threw rock 2 times, paper 4 times, and scissors 4 times. Bianca threw rock 3 times, paper 1 time, and scissors 6 times. What was the score?",
     default: true,
     puzzleName: "logic",
     student: null,
@@ -120,18 +124,18 @@ As it turns out, the one who ate the cookie is lying. Who ate the cookie?
     populateID: "logic11",
     puzzleId: "hats",
     title: "Hats",
-    description: "Three people are lined up so that they can only see the people standing in front of them. There are a total of 5 hats, 2 are black, 3 are white. Each person has one of the hats put on their head without seeing what color it is. Starting from the back they all try to guess what color hat they’re wearing.
-/n/n
+    description: `Three people are lined up so that they can only see the people standing in front of them. There are a total of 5 hats, 2 are black, 3 are white. Each person has one of the hats put on their head without seeing what color it is. Starting from the back they all try to guess what color hat they’re wearing.
+
 The last person in the line (who can see the two people in front of them) says, “It’s impossible for me to figure out what color hat I’m wearing.”
-/n/n
-	  The next person in the line (who can only see the one person in front of them) says, “It’s impossible for me to figure out what color hat I’m wearing.”
-/n/n
+
+    The next person in the line (who can only see the one person in front of them) says, “It’s impossible for me to figure out what color hat I’m wearing.”
+
 The next person in the line (who can’t see anybody else) says, “I know what color hat I’m wearing”.
-/n/n
+
 What color is their hat?
-",
+`,
     default: true,
     puzzleName: "logic",
     student: null,
   },
-]
+];
