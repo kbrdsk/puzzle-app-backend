133c67b8670fb69dc99284d7cd88ee064bfff53a
Kabirdas Henry <kbrdsk@gmail.com>
2020-11-23 18:56:58 -0800

    added defaults for light puzzles

--- defaults/light.js
+++ defaults/light.js
@@ -1,1 +1,16 @@
-module.exports.defaults = [];
+module.exports.defaults = [
+	{"puzzleId":"beginner1","title":"Beginner 1","size":{"cols":5,"rows":1},"beginstate":[],"default":true,"puzzleName":"light","student":null,"populateID":"WEqLS"},
+	{"puzzleId":"beginner2","title":"Beginner 2","size":{"cols":5,"rows":1},"beginstate":[{"row":0,"col":1},{"row":0,"col":3}],"default":true,"puzzleName":"light","student":null,"populateID":"XKtvQ"},
+	{"puzzleId":"beginner3","title":"Beginner 3","size":{"cols":5,"rows":1},"beginstate":[{"row":0,"col":2}],"default":true,"puzzleName":"light","student":null,"populateID":"tWaet"},
+	{"puzzleId":"beginner4","title":"Beginner 4","size":{"cols":3,"rows":1},"beginstate":[{"row":0,"col":1}],"default":true,"puzzleName":"light","student":null,"populateID":"QuYJB"},
+	{"puzzleId":"beginner5","title":"Beginner 5","size":{"cols":2,"rows":2},"beginstate":[],"default":true,"puzzleName":"light","student":null,"populateID":"7TB5O"},
+	{"puzzleId":"intermediate1","title":"Intermediate 1","size":{"cols":3,"rows":3},"beginstate":[],"default":true,"puzzleName":"light","student":null,"populateID":"BDcvc"},
+	{"puzzleId":"intermediate2","title":"Intermediate 2","size":{"cols":3,"rows":3},"beginstate":[{"row":1,"col":1}],"default":true,"puzzleName":"light","student":null,"populateID":"wHg6N"},
+	{"puzzleId":"intermediate3","title":"Intermediate 3","size":{"cols":3,"rows":3},"beginstate":[{"row":1,"col":0},{"row":1,"col":1},{"row":1,"col":2}],"default":true,"puzzleName":"light","student":null,"populateID":"Ht6N1"},
+	{"puzzleId":"intermediate4","title":"Intermediate 4","size":{"cols":3,"rows":3},"beginstate":[{"row":0,"col":0},{"row":1,"col":1},{"row":2,"col":0},{"row":2,"col":2},{"row":0,"col":2}],"default":true,"puzzleName":"light","student":null,"populateID":"bQTSJ"},
+	{"puzzleId":"intermediate5","title":"Intermediate 5","size":{"cols":3,"rows":3},"beginstate":[{"row":0,"col":0},{"row":0,"col":1},{"row":1,"col":1},{"row":2,"col":1},{"row":2,"col":2}],"default":true,"puzzleName":"light","student":null,"populateID":"IF4Oh"},
+	{"puzzleId":"expert1","title":"Expert 1","size":{"cols":5,"rows":5},"beginstate":[{"row":2,"col":2}],"default":true,"puzzleName":"light","student":null,"populateID":"K5wLA"},
+	{"puzzleId":"expert2","title":"Expert 2","size":{"cols":5,"rows":5},"beginstate":[{"row":4,"col":4},{"row":4,"col":3},{"row":4,"col":2},{"row":4,"col":1},{"row":4,"col":0},{"row":3,"col":0},{"row":3,"col":2},{"row":3,"col":1},{"row":3,"col":3},{"row":3,"col":4},{"row":2,"col":4},{"row":2,"col":3},{"row":2,"col":2},{"row":1,"col":3}],"default":true,"puzzleName":"light","student":null,"populateID":"iOr3s"},
+	{"puzzleId":"expert3","title":"Expert 3","size":{"cols":5,"rows":5},"beginstate":[],"default":true,"puzzleName":"light","student":null,"populateID":"y8RCL"},
+	
+];
